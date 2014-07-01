6994177b92ff1ea5d8e3d711a16c91c8b6a9fd05
ddivernois <david-emmanuel.divernois@amadeus.com>
2014-07-01 17:06:28 +0200

    fix: follow more closely what is done in the atpackager plugin
    see https://github.com/ariatemplates/noder-js/blob/master/build/builders/NoderPackage.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var through = require('through2');
 var findRequires = require('noder-js/findRequires');
+var moduleFunction = String(require('noder-js/context').prototype.jsModuleEval("$CONTENT$"));
 var path = require('path');
 
 module.exports.package = function (basePath) {
 
     return through.obj(function (file, encoding, done) {
         var fileContent = String(file.contents);
-        var requires = findRequires(fileContent);
-        var requiresStr = requires.length ? '"' + requires.join('", "') + '"' : '';
+        var requires = findRequires(fileContent, true);
         var filePath = path.relative(__dirname + '/../..' + (basePath || ''), file.path);
-        file.contents = new Buffer([
-            '\tdefine("' + filePath + '", [' + requiresStr + '], function(module, global) {',
-            '\t\tvar require = module.require, exports = module.exports, __filename = module.filename, __dirname = module.dirname;',
-            fileContent,
-            '\t});'].join('\n'));
-
+        file.contents = new Buffer(['define(', JSON.stringify(filePath), ', ', JSON.stringify(requires) + ', ',
+                moduleFunction.replace("$CONTENT$", fileContent), ');'].join(''));
         this.push(file);
         done();
     });
